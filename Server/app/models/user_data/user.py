0589b9d2b6842f9aa4ab74caf8b5fe23c1d81c57
devArtoria <contact@artoria.us>
2018-06-08 22:10:19 +0900

    UPDATE: by ERD changing

--- Server/app/models/user_data/user.py
+++ Server/app/models/user_data/user.py
@@ -1,10 +1,11 @@
 from app.models import db
+from sqlalchemy.dialects.mysql import INTEGER as Integer
 
 
 class User(db.Model):
     __tablename__ = 'user'
 
-    user_id = db.Column(db.Integer, db.ForeignKey("unuser.code"), primary_key=True)
+    user_id = db.Column(Integer(unsigned=True), primary_key=True, autoincrement=True)
     email = db.Column(db.VARCHAR)
     password = db.Column(db.VARCHAR)
     create_at = db.Column(db.TIMESTAMP)
