bb1b7c2f6b205cacad9860bb2740199ca0d4994e
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-07-09 16:18:17 +0900

    UPDATE: applying DB Column change

--- Server/app/models/question.py
+++ Server/app/models/question.py
@@ -6,4 +6,6 @@ class Question(db.Model):
     __tablename__ = "question"
 
     question_id = db.Column(Integer(unsigned=True), autoincrement=True, primary_key=True)
+    title = db.Column(db.VARCHAR(60))
+    body = db.Column(db.VARCHAR(1023))
     form = db.Column(db.JSON)
