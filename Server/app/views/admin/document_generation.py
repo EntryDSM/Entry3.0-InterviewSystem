6ee24dd6be2641b5ac6097d0ee3c816ad205d9f3
Lewis Kim (Sunghyun Kim) <kshr2d2@gmail.com>
2018-07-09 16:18:50 +0900

    IMPLEMENT: Question document generator logic

---
+++ Server/app/views/admin/document_generation.py
@@ -0,0 +1,26 @@
+from flask import (Blueprint, request)
+from flask_restful import Api
+from app.views import BaseResource, auth_required
+
+from app.models.question import Question
+from app.models import db
+
+api = Api(Blueprint('document generation', __name__))
+api.prefix = '/admin'
+
+
+@api.resource('/new')
+class New(BaseResource):
+    @auth_required
+    def post(self):
+        request_data = request.json
+
+        title = request_data["title"]
+        body = request_data["body"]
+        form = request_data["form"]
+
+        new_question = Question(title=title, body=body, form=form)
+        db.session.add(new_question)
+        db.session.commit()
+
+        return 200
