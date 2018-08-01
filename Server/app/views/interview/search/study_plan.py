613a3f73d2aab6d98e3ddeb6eb3f57be9be7ec4c
devArtoria <contact@artoria.us>
2018-08-02 08:33:25 +0900

    UPDATE: swagger

--- Server/app/views/interview/search/study_plan.py
+++ Server/app/views/interview/search/study_plan.py
@@ -2,6 +2,9 @@ from flask import (Blueprint, request)
 from flask_restful import Api
 from app.views import BaseResource, admin_required
 
+from app.docs.interview.study_plan import STUDY_PLAN
+from flasgger import swag_from
+
 from app.models.info import Info
 
 api = Api(Blueprint('study plan', __name__))
@@ -11,6 +14,7 @@ api.prefix = '/search'
 @api.resource('/<int:exam_code>/plan')
 class StudyPlan(BaseResource):
     @admin_required
+    @swag_from(STUDY_PLAN)
     def get(self, exam_code):
         study_plan = Info.query.filter_by(exam_code=exam_code).first().study_plan
 
