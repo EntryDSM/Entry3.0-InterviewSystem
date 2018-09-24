84f0c5317a7f9377dc257b0bb87283f389a81c0f
Lewis <artoria@artoria.us>
2018-09-24 14:24:24 +0900

    DELETE: old orm mapping

--- Server/app/models/interview_final.py
+++
@@ -1,9 +0,0 @@
-from app.models import db
-from sqlalchemy.dialects.mysql import INTEGER as Integer
-
-
-class InterviewFinal(db.Model):
-    __tablename__ = "interview_final"
-
-    user_id = db.Column(db.String(32), db.ForeignKey("user.user_id", ondelete='CASCADE'), primary_key=True)
-    final_score = db.Column(Integer(unsigned=True), nullable=True)
