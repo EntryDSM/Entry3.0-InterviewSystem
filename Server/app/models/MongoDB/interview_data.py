d2599942901afad6e368b05df4759226dd03f62d
Lewis <artoria@artoria.us>
2018-10-01 19:50:32 +0900

    UPDATE: MongoDB structure

--- Server/app/models/MongoDB/interview_data.py
+++ Server/app/models/MongoDB/interview_data.py
@@ -1,7 +1,27 @@
 from mongoengine import *
 
-class InterviewData(Document):
-    pass
 
-class InterviewQuestion(Document):
-    pass
+class StudentData(Document):
+    exam_code = StringField(primary_key=True)
+    plan_test = DictField()
+    passion_test = DictField()
+    understand_test = DictField()
+    possibility_test = DictField()
+
+    interview_score = FloatField()
+    conversion_score = FloatField()
+
+    is_take = BooleanField(default=False)
+
+
+class CodeTestData(Document):
+    exam_code = StringField(primary_key=True)
+    lang_use = DictField()
+    algorithm = DictField()
+    creativity = DictField()
+
+    code_score = FloatField()
+
+    is_take = BooleanField(default=True)
+
+
